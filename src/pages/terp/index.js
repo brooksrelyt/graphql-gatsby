d3dfc2f2eb7fff0bc396ff7a0c1ad5df44d486c9
Tyler Brooks <brooksrelyt@gmail.com>
2019-04-25 14:10:12 -0400

    update now I need to link cards to articles

--- src/pages/terp/index.js
+++ src/pages/terp/index.js
@@ -14,19 +14,38 @@ export default ({ data }) => (
     <div className="container mb-5">
       <div className="row mb-5">
         <div className="col-12">
-          <img className="img-fluid med-spaces" src="https://via.placeholder.com/1800x600" alt=" " />
+          {data.umdHub.articles.data.slice(6, 7).map((article) => (
+          <div class="hero-wrapper half clearfix">
+              {article.hero_image.map((hero, i) => (
+                <div key={i}>
+                  <div id="hero" class="header-banner" style={{backgroundImage: `url(${hero.url_1920_1080})`}}></div>
+                </div>
+              ))}
+              <div class="container h-textarea">
+                  <div class="row">
+                      <div class="col-lg-12 columns h-textarea-inner">
+                          <div class="textarea-intro">
+                          <br />
+                              <h2 class="white uppercase medium center"><strong>{ article.title }</strong></h2>
+                              <h4 class="white subheader georgia center"><em>{ article.summary }</em></h4>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          ))}
         </div>
       </div>
 
       <div className="row">
         <section className="col-md-9">
           <div className="row">
-            {data.umdHub.articles.data.slice(0, 10).map((article, i) => (
+            {data.umdHub.articles.data.slice(0, 6).map((article) => (
             <div className="col-md-6">
               <Card className="mb-5">
                 <a href="/">
                 {article.hero_image.map((hero, i) => (
-                  <div>
+                  <div key={i}>
                     <CardImg top width="100%" src={hero.url_1200_630} alt=" " />
                   </div>
                 ))}
@@ -38,7 +57,7 @@ export default ({ data }) => (
                   </a>
                   <CardText>{article.summary}</CardText>
                   <CardText className="footer">
-                    <span><a className="comment" href="/">0 Comment</a></span><span className="date">February 21, 2019</span>
+                    <span><a className="comment" href="/">0 Comment</a></span><span className="date">{article.authorship_date.formatted_short}</span>
                   </CardText>
                 </CardBody>
               </Card>
@@ -49,77 +68,44 @@ export default ({ data }) => (
         <aside className="col-md-3">
           <div>
             <ListGroup flush>
-              <ListGroupItem disabled tag="a" href="#">Recent Posts</ListGroupItem>
-              <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
-              <span>February 27, 2019</span>
-              <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
-              <span>February 27, 2019</span>
-              <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
-              <span>February 27, 2019</span>
-              <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
-              <span>February 27, 2019</span>
+              <ListGroupItem className="title" disabled tag="a" href="#">Recent Posts</ListGroupItem>
+              {data.umdHub.articles.data.slice(10,14).map((article, i) => (
+                <div key={i}>
+                  <ListGroupItem tag="a" href="#">{article.title}</ListGroupItem>
+                  <span>{article.authorship_date.formatted_short}</span>
+                </div>
+              ))}
             </ListGroup>
           </div>
 
           <div>
             <ListGroup flush>
-              <ListGroupItem disabled tag="a" href="#">Web Only</ListGroupItem>
-              <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
-              <span>February 27, 2019</span>
-              <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
-              <span>February 27, 2019</span>
-              <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
-              <span>February 27, 2019</span>
-              <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
-              <span>February 27, 2019</span>
+              <ListGroupItem className="title" disabled tag="a" href="#">Web Only</ListGroupItem>
+              {data.umdHub.articles.data.slice(15,19).map((article, i) => (
+                <div key={i}>
+                  <ListGroupItem tag="a" href="#">{article.title}</ListGroupItem>
+                  <span>{article.authorship_date.formatted_short}</span>
+                </div>
+              ))}
             </ListGroup>
           </div>
 
           <div>
             <ListGroup flush>
-              <ListGroupItem disabled tag="a" href="#">Popular Posts</ListGroupItem>
-              <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
-              <span>February 27, 2019</span>
-              <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
-              <span>February 27, 2019</span>
-              <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
-              <span>February 27, 2019</span>
-              <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
-              <span>February 27, 2019</span>
+              <ListGroupItem className="title" disabled tag="a" href="#">Popular Posts</ListGroupItem>
+              {data.umdHub.articles.data.slice(20,24).map((article, i) => (
+                <div key={i}>
+                  <ListGroupItem tag="a" href="#">{article.title}</ListGroupItem>
+                  <span>{article.authorship_date.formatted_short}</span>
+                </div>
+              ))}
             </ListGroup>
           </div>
         </aside>
       </div>
     </div>
 
-
     <Footer />
-    <div className="container spaces">
-      <div className="row">
-        <div className="col">
-          <h2>A list of Articles generated using graphql (no links yet)</h2>
-        </div>
-      </div>
-      <div className="row med-spaces">
-      {data.umdHub.articles.data.slice(0, 10).map((article, i) => (
-        <div className="col-4">
-          <Card key={i}>
-            {article.hero_image.map((hero, i) => (
-              <div>
-                <CardImg top width="100%" src={hero.url_1200_630} alt="Card image cap" />
-              </div>
-            ))}
-            <CardBody>
-              <CardTitle>{article.title}</CardTitle>
-              <CardSubtitle>{article.subtitle}</CardSubtitle>
-              <hr />
-              <CardText>{article.summary}</CardText>
-            </CardBody>
-          </Card>
-        </div>
-      ))}
-      </div>
-    </div>
   </div>
 )
 
@@ -137,6 +123,15 @@ export const GatsbyQuery = graphql`
           hero_image {
             url_1200_630
           }
+          authorship_date {
+            formatted_short
+            unix
+            unix_int
+            formatted_long
+            formatted_short
+            time
+          }
+          slug
         }
       }
     }
